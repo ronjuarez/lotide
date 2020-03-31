1f809b3e64cbb3f256c246d369192b399fb34667
Ron Juarez <ronstewartjuarez@gmail.com>
2020-03-31 01:34:53 +0000

    function that creats a new array out of an exsisting one by removing the firsst element

---
+++ tail.js
@@ -0,0 +1,24 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log('✅✅✅ Assertion Passed: [actual] === [expected]');
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+
+  }
+
+};
+
+let tail = function(array) {
+  let newArray = array.slice(1);
+  
+  return newArray;
+};
+
+const result = tail(["Hello", "Lighthouse", "Labs"]);
+assertEqual(result.length, 2);
+assertEqual(result[0], "Lighthouse");
+assertEqual(result[1], "Labs");
+
+const words = ["Yo Yo", "Lighthouse", "Labs"];
+tail(words);
+assertEqual(words.length, 3);
