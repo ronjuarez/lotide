73cee68905cb9b70708a43dcc8a59bf4ede5874f
Ron Juarez <ronstewartjuarez@gmail.com>
2020-04-06 20:09:46 +0000

    this is a refactored head function that requires assertequal

--- head.js
+++ head.js
@@ -1,12 +1,4 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log('✅✅✅ Assertion Passed: [actual] === [expected]');
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
-
-  }
-
-};
+const assertEqual = require('./assertEqual');
 
 let head = function(array) {
   let result = array[0];
@@ -15,6 +7,3 @@ let head = function(array) {
 
 };
 
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-assertEqual(head([]), undefined);
