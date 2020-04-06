1a1cd11a99e3ea74fb6b071acadcf2fcafc778f8
Ron Juarez <ronstewartjuarez@gmail.com>
2020-04-06 20:11:04 +0000

    this is a test that runs assert equal

---
+++ test/assertEqualTest.js
@@ -0,0 +1,9 @@
+const assertEqual = require('../assertEqual');
+
+
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+assertEqual('March','March');
+assertEqual('Ron', 'Ronny');
+assertEqual(25, 25);
+assertEqual(100, 1000);
