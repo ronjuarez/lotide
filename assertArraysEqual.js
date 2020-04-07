d36f02e5de7a8902680910ea232a9347349cc9ab
Ron Juarez <ronstewartjuarez@gmail.com>
2020-04-07 01:20:55 +0000

    assert arrays equals function with console logged declaration of the result

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -20,8 +20,4 @@ let assertArraysEqual = function (arrayOne, arrayTwo) {
   }
 }
 
-assertArraysEqual([1, 2, 3], [1, 2, 3]);
-assertArraysEqual([1, 2, 3], [3, 2, 1]);
-
-assertArraysEqual(["1", "2", "3"], ["1", "2", "3"]);
-assertArraysEqual(["1", "2", "3"], ["1", "2", 3]);
+module.exports = assertArraysEqual;
